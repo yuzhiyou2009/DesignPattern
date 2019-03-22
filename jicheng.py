023ee98de08b022a02629ab6b4a5f383c36d0883
yuzhiyou2009 <yuzhiyou@gmail.com>
2019-03-22 23:36:28 +0800

    Add files via upload

---
+++ jicheng.py
@@ -0,0 +1,48 @@
+import os
+
+class AbstractGun():
+    gunname = ""
+    def shoot(self):
+        print("this is gun")
+        pass
+
+class Soldier():
+    gun = AbstractGun()
+
+    def setGun(self, gun):
+        self.gun = gun
+
+    def killEnemy(self):
+        self.gun.shoot()
+        pass
+
+class Handgun(AbstractGun):
+    def shoot(self):
+        print("this is handgun")
+
+class Rifle(AbstractGun):
+    def shoot(self):
+        print("this is rifle")
+
+class Machinegun(AbstractGun):
+    def shoot(self):
+        print("this is machinegun")
+
+class Snipper(Soldier):
+    def zoomDis(self):
+        print("snipper look up!")
+
+class AWP(Rifle):
+    def shoot(self):
+        print("this is AWP!")
+
+if __name__ == '__main__':
+    newSoldier = Soldier()
+    newSoldier.setGun(AbstractGun())
+    newSoldier.killEnemy()
+
+    newSnipper = Snipper()
+    newSnipper.setGun(AWP())
+
+    newSnipper.zoomDis()
+    newSnipper.killEnemy()
